877b0ca52003d959c1694c24fddb313757e62ee1
Huy Le <huylh@bap.jp>
2021-05-10 11:48:57 +0700

    - Update code styles

--- RxSwiftApp/Utitlities/UIApplication+Extensions.swift
+++ RxSwiftApp/Utitlities/UIApplication+Extensions.swift
@@ -10,10 +10,8 @@ extension UIApplication {
     ) -> UIViewController? {
         if let nav = base as? UINavigationController {
             return getTopViewController(base: nav.visibleViewController)
-
         } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
             return getTopViewController(base: selected)
-
         } else if let presented = base?.presentedViewController {
             return getTopViewController(base: presented)
         }
