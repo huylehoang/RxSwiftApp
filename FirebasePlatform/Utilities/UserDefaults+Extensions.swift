74df5645f33e2c9474afa3e04dae29fc7f36b053
Huy Le <huylh@bap.jp>
2021-05-31 15:40:42 +0700

    - Update code styles (UserDefaults)

--- FirebasePlatform/Utilities/UserDefaults+Extensions.swift
+++ FirebasePlatform/Utilities/UserDefaults+Extensions.swift
@@ -2,17 +2,21 @@ import RxSwift
 import Domain
 
 extension UserDefaults {
-    enum Key: ErrorType, CaseIterable {
+    enum Key: String, CaseIterable {
         case userPassword
     }
 
+    enum Error: ErrorType {
+        case userPasswordNotFound
+    }
+
     static func setValue(_ value: Any, forKey key: Key) {
-        standard.setValue(value, forKey: key.description)
+        standard.setValue(value, forKey: key.rawValue)
         standard.synchronize()
     }
 
     static func removeValue(forKey key: Key) {
-        standard.removeObject(forKey: key.description)
+        standard.removeObject(forKey: key.rawValue)
         standard.synchronize()
     }
 
@@ -23,8 +27,8 @@ extension UserDefaults {
 
     static func getStringValue(forKey key: Key) -> Single<String> {
         return .create { single in
-            guard let value = standard.string(forKey: key.description) else {
-                single(.failure(key))
+            guard let value = standard.string(forKey: key.rawValue) else {
+                single(.failure(key.error))
                 return Disposables.create()
             }
             single(.success(value))
@@ -33,22 +37,24 @@ extension UserDefaults {
     }
 }
 
-extension UserDefaults.Key: LocalizedError, CustomStringConvertible {
-    var description: String {
+extension UserDefaults.Key {
+    var error: UserDefaults.Error {
         switch self {
-        case .userPassword: return "userPassword"
-        }
-    }
-
-    var errorDescription: String? {
-        switch self {
-        case .userPassword: return "Missing sensitive data.\nPlease try to sign in again."
+        case .userPassword: return .userPasswordNotFound
         }
     }
 }
 
-extension UserDefaults.Key {
+extension UserDefaults.Error: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .userPasswordNotFound: return "Missing sensitive data.\nPlease try to sign in again."
+        }
+    }
+}
+
+extension UserDefaults.Error {
     var forceSignOut: Bool {
-        return self == .userPassword
+        return self == .userPasswordNotFound
     }
 }
