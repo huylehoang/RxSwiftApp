04494e7bcbe510279756b1924fc572236c122b0a
Huy Le <huylh@bap.jp>
2021-06-03 21:56:44 +0700

    - Update code styles

--- Application/Components/RxTableView.swift
+++ Application/Components/RxTableView.swift
@@ -2,13 +2,13 @@ import UIKit
 
 /// Use `RxTableView` to suppress the `AutoLayout` warning when bind `dataSource` early.
 class RxTableView: UITableView {
-  override func layoutSubviews() {
-    guard nil != window else { return }
-    super.layoutSubviews()
-  }
+    override func layoutSubviews() {
+        guard nil != window else { return }
+        super.layoutSubviews()
+    }
 
-  override func layoutIfNeeded() {
-    guard nil != window else { return }
-    super.layoutIfNeeded()
-  }
+    override func layoutIfNeeded() {
+        guard nil != window else { return }
+        super.layoutIfNeeded()
+    }
 }
